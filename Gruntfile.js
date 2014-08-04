6d4443302d0db2329ed41716c6296a8228fddec1
ryanmunger <ryanmunger@gmail.com>
2014-08-04 15:31:22 -0700

    Final indentation issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,6 +64,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.registerTask('default',['watch']);
 };
